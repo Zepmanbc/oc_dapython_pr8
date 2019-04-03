22055461e81d5c3c8c757b54f6044008957be74e
ZepmanBC <zepman@gmail.com>
2019-04-03 11:30:37 +0200

    mise en place des urls et des views

--- purbeurre/products/urls.py
+++ purbeurre/products/urls.py
@@ -1,7 +1,17 @@
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'products'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView, name='index'),
+    path('search/<str:query>/', views.SearchView, name='search'),
+    path('<int:product_id>/result/', views.ResultView, name='result'),
+    path('<int:product_id>/save/<int:substitute_id>',
+         views.SaveView, name='save'),
+    path('<int:product_id>/detail/', views.DetailView, name='detail'),
+    path('account/', views.AccountView, name='account'),
+    path('disconnect/', views.DisconnectView, name='disconnect'),
+    path('myproducts/', views.MyProductsView, name='myproducts'),
+    path('legal/', views.LegalView, name='legal'),
 ]
--- purbeurre/products/views.py
+++ purbeurre/products/views.py
@@ -2,7 +2,55 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
+def IndexView(request):
 
     context = {}
     return render(request, 'products/index.html', context)
+
+
+def SearchView(request, query):
+
+    context = {'query': query}
+    return render(request, 'products/search.html', context)
+
+
+def DetailView(request, product_id):
+
+    context = {}
+    return render(request, 'products/detail.html', context)
+
+
+def ResultView(request, product_id):
+
+    context = {}
+    return render(request, 'products/result.html', context)
+
+
+def AccountView(request):
+
+    context = {}
+    return render(request, 'products/account.html', context)
+
+
+def DisconnectView(request):
+
+    context = {}
+    return render(request, 'products/index.html', context)
+
+
+def LegalView(request):
+
+    context = {}
+    return render(request, 'products/legal.html', context)
+
+
+def SaveView(request, product_id, substitude_id):
+
+    context = {}
+    return render(request, 'products/index.html', context)
+
+
+def MyProductsView(request):
+
+    context = {}
+    return render(request, 'products/myproducts.html', context)
