01891180585eb048f472d5181e221c615bc06a26
ZepmanBC <zepman@gmail.com>
2019-04-05 20:01:06 +0200

    déplacement template et static commun vers statis et templates du repertoire projet

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'purbeurre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,3 +159,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
