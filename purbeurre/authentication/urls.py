4dcba7b6ad44549406c1391aa7aebfac1b33d0db
ZepmanBC <zepman@gmail.com>
2019-04-09 17:35:14 +0200

    simplification auth utilisation du username pour finaliser la fonctionalité

--- purbeurre/authentication/urls.py
+++ purbeurre/authentication/urls.py
@@ -2,16 +2,12 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
-from .forms import AuthenticationFormEmail
+# from .forms import AuthenticationFormEmail
 
 
 app_name = 'authentication'
 urlpatterns = [
-    # path('login/', views.LoginView, name='login'),
-    # path('login/',
-    #      auth_views.LoginView.as_view(form_class=AuthenticationFormEmail),
-    #      name='login'),
-    path('login/', views.LoginView.as_view(),name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView, name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('account/', views.AccountView, name='account'),
--- purbeurre/authentication/views.py
+++ purbeurre/authentication/views.py
@@ -3,26 +3,10 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import RegisterForm, LoginForm
-# from .models import User
+
+from .forms import RegisterForm
 
 # Create your views here.
-
-
-# def LoginView(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             email = form.cleaned_data.get('email')
-#             raw_password = form.cleaned_data.get('password')
-#             user = authenticate(email=email, password=raw_password)
-#             if user:
-#                 login(request, user)
-#                 return redirect('products:index')
-#     else:
-#         form = LoginForm()
-
-#     return render(request, 'authentication/login.html', {'form': form})
 
 
 def AccountView(request):
@@ -40,38 +24,16 @@ def LogoutView(request):
     return redirect('products:index')
 
 
-# def RegisterView(request):
-#     if request.method == 'POST':
-#         form = RegisterForm(request.POST)
-#         if form.is_valid():
-#             form.save()  # create user in DB
-
-#             email = form.cleaned_data.get('email')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(email=email, password=raw_password)
-#             login(request, user)
-
-#             return redirect('products:index')
-#     else:
-#         form = RegisterForm()
-#         return render(request, 'authentication/register.html', {'form': form})
-
-
-class RegisterView(generic.CreateView):
+class RegisterView(generic.FormView):
     form_class = RegisterForm
     success_url = reverse_lazy('products:index')
     template_name = 'authentication/register.html'
 
-
-class LoginView(generic.FormView):
-    form_class = LoginForm
-    success_url = reverse_lazy('products:index')
-    template_name = 'authentication/login.html'
-
     def form_valid(self, form):
-        email = form.cleaned_data.get('email')
-        raw_password = form.cleaned_data.get('password')
-        user = authenticate(email=email, password=raw_password)
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
         if user:
             login(self.request, user)
         return super().form_valid(form)
