9436f754f5f8a515c7b76addc9ef0acd402f6585
ZepmanBC <zepman@gmail.com>
2019-04-13 14:50:45 +0200

    Front améliorations

--- purbeurre/static/js/front.js
+++ purbeurre/static/js/front.js
@@ -15,6 +15,23 @@ window.onload = function(event) {
 }
 
 function resizeAll(){
-    var thumbWidth = document.getElementsByClassName('product-thumb')[0].offsetWidth;
-    $('.product-thumb').height(thumbWidth + 'px')
+    if($('.product-thumb')[0]){
+        var thumbWidth = document.getElementsByClassName('product-thumb')[0].offsetWidth;
+        $('.product-thumb').height(thumbWidth + 'px')
+    }
+    // console.log('.documentElement.scrollHeight = ' + document.documentElement.scrollHeight);
+    // console.log('.body.clientHeight = ' + document.body.clientHeight);
+    // console.log('window.outerHeight = ' + window.outerHeight);
+    if (document.documentElement.scrollHeight > document.body.clientHeight + 41){
+        document.querySelector('footer').style.position = 'relative'
+    }else{
+        document.querySelector('footer').style.position = 'absolute'
+    }
+    if (document.documentElement.scrollWidth < 480){
+        $('.fa-arrow-right').removeClass('fa-4x').addClass('fa-2x')
+        $('.fa-trash-alt').removeClass('fa-4x').addClass('fa-2x')
+    }else{
+        $('.fa-arrow-right').removeClass('fa-2x').addClass('fa-4x')
+        $('.fa-trash-alt').removeClass('fa-2x').addClass('fa-4x')
+    }
 }
