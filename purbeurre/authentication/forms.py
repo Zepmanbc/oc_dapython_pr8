fd7b079d111353d2102a6cbf52bf5179b692f715
ZepmanBC <zepman@gmail.com>
2019-04-13 17:41:16 +0200

    creation du user alternatif avec email

--- purbeurre/authentication/forms.py
+++ purbeurre/authentication/forms.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from .models import User
 
 
 class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2')
