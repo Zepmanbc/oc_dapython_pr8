6be87e0fdaaf5c6be899098c0dc0e65bdd28f8b7
ZepmanBC <zepman@gmail.com>
2019-04-17 16:41:50 +0200

    Heroku modif static settings

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -174,19 +174,19 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 if os.environ.get('ENV') == 'PRODUCTION':
 
-    # # Static files settings
-    # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-    # # Extra places for collectstatic to find static files.
-    # STATICFILES_DIRS = (
-    #     os.path.join(PROJECT_ROOT, 'static'),
-    # )
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
