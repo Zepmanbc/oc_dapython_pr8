e91b5fb4b83555ed88cfcb3e86a52773342cea1b
ZepmanBC <zepman@gmail.com>
2019-04-18 19:07:42 +0200

    travis config pour postgres

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -100,11 +100,15 @@ if get_env_variable('ENV') == 'PRODUCTION':
 elif get_env_variable('ENV') == 'TRAVIS':
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:'
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisdb',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
         }
     }
-    
+
 else:
     from django.db.backends.mysql.base import DatabaseWrapper
     DatabaseWrapper.data_types['DateTimeField'] = 'datetime'
