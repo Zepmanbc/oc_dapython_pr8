8b38d8984d24c6e6d75e65a8c10d58600a45d314
ZepmanBC <zepman@gmail.com>
2019-04-19 23:08:09 +0200

    admin ajout de User

--- purbeurre/authentication/admin.py
+++ purbeurre/authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User
+
+
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ['email']
+    list_display = ('email', 'first_name', 'last_name', 'last_login',)
