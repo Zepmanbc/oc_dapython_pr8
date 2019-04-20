486bc4e3932c2465141c0abecfaf9e6795cb80e5
ZepmanBC <zepman@gmail.com>
2019-04-20 12:22:55 +0200

    front improvement

--- purbeurre/static/js/front.js
+++ purbeurre/static/js/front.js
@@ -1,11 +1,3 @@
-// window.onload = function(event) {
-//     body = document.body;
-//     html = document.documentElement;
-//     var footer = document.querySelector('footer');
-//     footer.style.position = "absolute";
-//     footer.style.left = x_pos+'px';
-//     footer.style.top = y_pos+'px';
-// }
 window.onresize = function(event) {
     resizeAll();
 };
@@ -15,10 +7,6 @@ window.onload = function(event) {
 }
 
 function resizeAll(){
-    if($('.product-thumb')[0]){
-        var thumbWidth = document.getElementsByClassName('product-thumb')[0].offsetWidth;
-        $('.product-thumb').height(thumbWidth + 'px')
-    }
     // console.log('.documentElement.scrollHeight = ' + document.documentElement.scrollHeight);
     // console.log('.body.clientHeight = ' + document.body.clientHeight);
     // console.log('window.outerHeight = ' + window.outerHeight);
@@ -26,12 +14,5 @@ function resizeAll(){
         document.querySelector('footer').style.position = 'relative'
     }else{
         document.querySelector('footer').style.position = 'absolute'
-    }
-    if (document.documentElement.scrollWidth < 480){
-        $('.fa-arrow-right').removeClass('fa-4x').addClass('fa-2x')
-        $('.fa-trash-alt').removeClass('fa-4x').addClass('fa-2x')
-    }else{
-        $('.fa-arrow-right').removeClass('fa-2x').addClass('fa-4x')
-        $('.fa-trash-alt').removeClass('fa-2x').addClass('fa-4x')
     }
 }
